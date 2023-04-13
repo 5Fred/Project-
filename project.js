3fd1313c4dd1523f65f3f498a6ed6e2657118352
Fredrick <mwangikingz94@gmail.com>
2023-04-13 16:10:13 +0300

    almost

--- project.js
+++ project.js
@@ -10,30 +10,94 @@
 //     document.getElementById('root').
 //     innerHTML=completedata[2].title;
 // });
-const searchBar = document.getElementById('search-btn')
-const mysearchBtn = document.getElementById('root');
-const driverList = document.getElementById('container');
 
-searchBar.addEventListener('click', getDriver);
 
-function getDriver(){
-  let finding = document.getElementById('search-btn').value;
-  fetch('http://ergast.com/api/f1/drivers.json')
+// const searchBar = document.getElementById('search-bar');
+// const searchBtn = document.getElementById('search-btn');
+// const driverList = document.getElementById('driver-list');
+
+// searchBtn.addEventListener('click', getDrivers);
+
+// function getDrivers() {
+//   let searchQuery = searchBar.value;
+//   fetch(`http://ergast.com/api/f1/${searchQuery}/drivers.json`)
+//     .then(response => response.json())
+//     .then(data => {
+//       let driversHtml = "";
+//       data.MRData.DriverTable.Drivers.forEach(driver => {
+//         driversHtml += `
+//           <div class="card">
+//             <h3>${driver.givenName} ${driver.familyName}</h3>
+//             <p>Nationality: ${driver.nationality}</p>
+//             <a href="${driver.url}">Click Here</a>
+//           </div>
+//         `;
+//       });
+//       driverList.innerHTML = driversHtml;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       driverList.innerHTML = "<p>Failed to retrieve drivers</p>";
+//     });
+// }
+
+
+// const searchBtn = document.getElementById("search-btn");
+// const searchInput = document.getElementById("search-bar");
+// const driverList = document.getElementById("driver-list");
+
+// searchBtn.addEventListener("click", function() {
+//   const searchText = searchInput.value;
+//   let searchQuery = searchText;
+//   fetch(`http://ergast.com/api/f1/${searchQuery}/drivers.json`)
+//     .then(response => response.json())
+//     .then(data => {
+//       let driversHtml = "";
+//       data.MRData.DriverTable.Drivers.forEach(driver => {
+//         driversHtml += `
+//           <div class="card">
+//             <h3>${driver.givenName} ${driver.familyName}</h3>
+//             <p>Nationality: ${driver.nationality}</p>
+//             <a href="${driver.url}">Click Here</a>
+//           </div>
+//         `;
+//       });
+//       driverList.innerHTML = driversHtml;
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       driverList.innerHTML = "<p>Failed to retrieve drivers</p>";
+//     });
+// });
+
+  // Do something with searchText, such as search a database or perform a web search
+
+
+  
+  const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const searchResults = document.getElementById('search-results');
+const searchBtn = document.getElementById("searchBtn");
+
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent form submission
+
+  const searchTerm = searchInput.value;
+  const apiUrl = `http://ergast.com/api/f1/drivers.json?search=${searchTerm}`;
+
+  fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        let myHtml = "";
-      // loop through each driver and create a card for them
-      data.Drivers.forEach(driver => {
-        myHtml +=`
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.innerHTML = 
-          <div class="card-details">
-            <h3>${driver.driverId}</h3>
-            <h4>${driver.url}</h4>
-          </div>
-        `;
+      // clear previous search results
+      searchResults.innerHTML = '';
+
+      const drivers = data.MRData.DriverTable.Drivers;
+      drivers.forEach(driver => {
+        // create a new element to display each driver's name
+        const driverElement = document.createElement('div');
+        driverElement.textContent = `${driver.givenName} ${driver.familyName}`;
+        searchResults.appendChild(driverElement);
       });
-      driverList.innerHTML+= myHtml;
-    });
-}
+    })
+    .catch(error => console.error(error));
+});
